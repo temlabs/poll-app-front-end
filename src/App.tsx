235cd0f49fb3ec9fc0dfddd17070e213d92b8370
temlabs <temilabinjo@hotmail.co.uk>
2022-04-09 12:56:52 +0100

    Added header and moved the maincontent in App into a variable for clarity

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import CreatePoll from "./components/CreatePoll";
 import VoteInPoll from "./components/VoteInPoll";
 import "./styles/main-style.css";
 function App(): JSX.Element {
+  let mainContent: JSX.Element;
   if (window.location.href.match("#/polls/*")) {
-    return <VoteInPoll />;
+    mainContent = <VoteInPoll />;
   } else {
-    return <CreatePoll />;
+    mainContent = <CreatePoll />;
   }
+
+  return (
+    <>
+      <header className="flex-container-column centre-children">
+        <h2>
+         We can't make everything perfect, but we can make up our minds :) 
+        </h2>
+        
+      </header>
+      <section>
+        {mainContent}
+      </section>    
+    </>
+
+    
+  )
 }
 
 export default App;
