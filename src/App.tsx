b0010ccc2fec13570c72e417a67ec37fb0cb182d
temlabs <temilabinjo@hotmail.co.uk>
2022-04-18 16:30:11 +0100

    Added an icon for the tab as well as an improved header which features a logo. Some small adjustments to dimensions in the stylesheet too.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import CreatePoll from "./components/CreatePoll";
 import VoteInPoll from "./components/VoteInPoll";
 import "./styles/main-style.css";
+import logo from "./images/p-poll-head.png";
+
 function App(): JSX.Element {
   let mainContent: JSX.Element;
   if (window.location.href.match("#/polls/*")) {
@@ -11,11 +13,9 @@ function App(): JSX.Element {
 
   return (
     <>
-      {/* <header className="flex-container-column centre-children">
-        <h2>
-          We can't make everything perfect, but we can make up our minds :)
-        </h2>
-      </header> */}
+      <header className="flex-container-column centre-children">
+        <img src={logo} alt="p poll logo" />
+      </header>
       <section>{mainContent}</section>
     </>
   );
