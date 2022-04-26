acc1e07b4d782a212019ecf5a20c57a46af294ae
temlabs <temilabinjo@hotmail.co.uk>
2022-04-26 20:34:02 +0100

    Attempting router but will go back to history branch because I did more refactoring there

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import CreatePoll from "./components/CreatePoll";
 import VoteInPoll from "./components/VoteInPoll";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/main-style.css";
 function App(): JSX.Element {
-  let mainContent: JSX.Element;
-  if (window.location.href.match("#/polls/*")) {
-    mainContent = <VoteInPoll />;
-  } else {
-    mainContent = <CreatePoll />;
-  }
+
 
   return (
-    <>
-      <header className="flex-container-column centre-children">
-        <h2>
-          We can't make everything perfect, but we can make up our minds :)
-        </h2>
-      </header>
-      <section>{mainContent}</section>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CreatePoll/>}/>
+        <Route path="/polls" element={<VoteInPoll/>}/>
+      </Routes>
+    </Router>
   );
 }
 
